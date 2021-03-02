4ece8324efb53c8555072c6d9a382685b10253d8
Juan Agustín Zitelli <juanzitelli7@gmail.com>
2021-03-02 15:54:09 -0300

    Feature: Basic routing finished
    This addition includes the creation of the application's basic routing, including the Home & Movie Details page.
    	- Home: The application's main screen, where users will be able to find movie recommendations and search for specific movies, as well as filtering them based on a 5-star rating value.
    	- Movie Details: A screen dedicated to showing a single movie's details, like genre, release date, director and more.
    
    This increases the app's version to 1.1.0

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -3,14 +3,18 @@ import {
 	BrowserRouter as Router,
 	Switch,
 	Route,
+	Redirect,
 } from "react-router-dom";
+import Home from '../components/Home';
+import MovieDetails from '../components/MovieDetails';
 const AppRouter = () => {
 	return (
 		<Router>
 			<div>
 				<Switch>
-					<Route exact path="/" />
-					<Route path="/movie/:movieId" />
+					<Route exact path="/" component={Home} />
+					<Route path="/movie/:movieId" component={MovieDetails} />
+					<Redirect to="/" />
 				</Switch>
 			</div>
 		</Router >
